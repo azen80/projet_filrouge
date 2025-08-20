0176e73c5291d81b196998e41032b5d649fbd681
Quidé <quideclement@gmail.com>
2025-08-20 16:07:09 +0200

    commit

--- village_green/src/Controller/PanierController.php
+++ village_green/src/Controller/PanierController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,18 +12,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(Request $request): Response
+    public function index(Request $request, ProduitRepository $repo): Response
     {
+        $panier_complet = [];
         $session = $request->getSession();
 
         $panier = $session->get('panier', []);
         
+        foreach ($panier as $id => $quantite) {
+
+            $produit = $repo->find($id);
+            $panier_complet[] = [
+                'produit' => $produit,
+                'quantite' => $quantite
+            ];
+        }
+
 
         return $this->render('panier/index.html.twig', [
             'controller_name' => 'PanierController',
+            'panier' => $panier_complet
         ]);
     }
 
+
+
+    
 
     #[Route('/panier/add/{produit}', name: 'app_panier_add')]
     public function add(Produit $produit, Request $request): Response
